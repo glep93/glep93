7b0b6755aa8d48646cbc6a8e52d02a846ea629ee
Gelp93 <luca.glep.grandinetti@gmail.com>
2020-12-29 12:11:49 +0100

    dummy

--- js/modern-blog.js
+++ js/modern-blog.js
@@ -54,8 +54,9 @@ var demo = (function (window) {
             cell_size: 90,
             variance: 1,
             stroke_width: 1,
-            x_colors: 'random',
-            y_colors: 'random'
+            x_colors: 'Greys'
+          //  x_colors: 'random',
+          //  y_colors: 'random'
         }).svg(); // Render as SVG.
 
         _mapPolygons(pattern);
